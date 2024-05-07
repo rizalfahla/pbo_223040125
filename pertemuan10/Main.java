8cac6f7d907d178547137d582fb146ae901633e1
Rizal Fahla <75971110+rizalfahla@users.noreply.github.com>
2024-05-07 18:24:50 +0700

    update pertemuan 10

--- pertemuan10/Main.java
+++ pertemuan10/Main.java
@@ -2,7 +2,7 @@ package pertemuan10;
 
 public class Main {
     public static void main(String[] args) {
-        Dosen dosen = new Dosen("111", "Zuhdi", "Agama Islam");
+        Dosen dosen = new Dosen("111", "Aceng", "Agama Islam");
         System.out.println(dosen.getNip() + " - " + dosen.getNama() + " - " + dosen.getJurusan());
     }
 }
